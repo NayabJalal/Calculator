5c7251311cfdf9e3d5b4e8d010bdf7e48a754606
NayabJalal <nayabjalal786@gmail.com>
2025-06-24 00:58:13 +0530

    Upload Calculator App

---
+++ src/main/java/com/nayabjalal/calculator/App.java
@@ -0,0 +1,10 @@
+package com.nayabjalal.calculator;
+
+import com.nayabjalal.calculator.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
